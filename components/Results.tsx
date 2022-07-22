1681b6184c867cde9dd79415d9d1be7df208c1db
Mutesa-Cedric <mutesacedric@gmail.com>
2022-07-22 16:59:43 +0200

    fix : removed flip move

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,4 +1,3 @@
-import FlipMove from "react-flip-move";
 import Thumbnail from "./Thumbnail"
 
 interface ResultProps {
@@ -6,12 +5,12 @@ interface ResultProps {
 }
 function Results({ results }: ResultProps) {
     return (
-        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex
+        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex
         flex-wrap justify-center">
             {results.map((result: any) => (
                 <Thumbnail key={result.id} result={result} />
             ))}
-        </FlipMove>
+        </div>
     )
 }
 
